898fab1780a83fb93f1ed8449551aa76c8311160
Paul Chyz <paulhchyz@gmail.com>
2022-09-16 11:49:18 -0700

    Initial commit

---
+++ func.py
@@ -0,0 +1,100 @@
+# Oracle Cloud Infrastructure Streaming Pipeline Function
+# Created by Paul Chyz on 1/25/2022
+
+import io
+import json
+import logging
+import base64
+import datetime
+import pandas as pd
+import oci.object_storage
+import requests
+
+def handler(ctx, data: io.BytesIO=None):
+    try:
+        processed_bucket = "streaming_bucket_processed"
+        cfg = ctx.Config()
+        ordsbaseurl = cfg['ords-base-url']
+        schema = cfg['db-schema']
+        dbuser = cfg['db-user']
+        dbpwd = cfg['dbpwd-cipher']
+        client, namespace = config_object_store()
+        src_objects = json.loads(data.getvalue().decode('utf-8'))
+        output = execute_etl(client, namespace, processed_bucket, src_objects, ordsbaseurl, schema, dbuser, dbpwd)
+        
+    except (Exception, ValueError) as ex:
+        logging.getLogger().info('error: ' + str(ex))
+    return output
+
+def config_object_store():
+    signer = oci.auth.signers.get_resource_principals_signer()
+    client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
+    namespace = client.get_namespace().data
+    return client, namespace
+
+def execute_etl(client, namespace, dst_bucket, src_objects, ordsbaseurl, schema, dbuser, dbpwd):
+    decoded_objects = decode_objects(src_objects)
+    csv_data = to_csv(decoded_objects)
+    obj_name = 'csv_data/' + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f') + '.csv'
+    resp = put_object(client, namespace, dst_bucket, obj_name, csv_data)
+    load_resp = load_data(ordsbaseurl, schema, dbuser, dbpwd, decoded_objects)
+    return decoded_objects
+
+def decode_objects(src_objects):
+    for obj in src_objects:
+            obj['key'] = base64.b64decode(obj['key']).decode('utf-8')
+            obj['value'] = json.loads(base64.b64decode(obj['value']).decode('utf-8'))
+    return src_objects
+
+def to_csv(data_list):
+    for item in data_list:
+        for key in item['value'][0]:
+            item[key] = item['value'][0][key]
+        del item['value']
+
+    df = pd.json_normalize(data_list)
+    csv_data = df.to_csv(index=False)
+    return csv_data
+
+def put_object(client, namespace, dst_bucket, obj_name, data):
+    try:
+        output = client.put_object(namespace_name=namespace, bucket_name=dst_bucket, object_name=obj_name, put_object_body=data, content_type="text/csv")
+    except (Exception, ValueError) as ex:
+        logging.getLogger().error(str(ex))
+        return {"Error Response": str(ex)}
+    return output
+
+def load_data(ordsbaseurl, schema, dbuser, dbpwd, decoded_objects):
+    for obj in decoded_objects:
+        insert_status = soda_insert(ordsbaseurl, schema, dbuser, dbpwd, obj)
+        if "id" in insert_status["items"][0]:
+            print("INFO - Successfully inserted document ID " + insert_status["items"][0]["id"], flush=True)
+        else:
+            raise SystemExit("Error while inserting: " + insert_status)
+    return insert_status
+
+def soda_insert(ordsbaseurl, schema, dbuser, dbpwd, obj):
+    auth=(dbuser, dbpwd)
+    sodaurl = ordsbaseurl + schema + '/soda/latest/'
+    collectionurl = sodaurl + "streamdata"
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(collectionurl, auth=auth, headers=headers, data=json.dumps(obj))
+    r_json = {}
+    try:
+        r_json = json.loads(r.text)
+    except ValueError as e:
+        print(r.text, flush=True)
+        raise
+    return r_json
+
+def update_tables(ordsbaseurl, schema, dbuser, dbpwd, procedure):
+    auth=(dbuser, dbpwd)
+    procedure_url = ordsbaseurl + schema + '/' + procedure + '/'
+    r = requests.post(procedure_url, auth=auth)
+    r_json = {}
+    try:
+        r_json = json.loads(r.text)
+    except ValueError as e:
+        print(r.text, flush=True)
+        raise
+    return r_json
---
+++ stream.py
@@ -0,0 +1,112 @@
+# Oracle Cloud Infrastructure IoT Streaming Simulation Script
+# Created by Paul Chyz on 5/19/2021
+
+import oci
+import json
+import time
+import datetime
+import random
+from base64 import b64encode
+
+# Create a default config using DEFAULT profile in default location
+config = oci.config.from_file()
+sid = 'STREAM_ID'
+message_endpoint = 'MESSAGE_ENDPOINT'
+
+amp_odds = .01
+freq_odds = .01
+temp_odds = .01
+hum_odds = .01
+
+amp_flag = False
+freq_flag = False
+temp_flag = False
+hum_flag = False
+
+amp_count = 0
+freq_count = 0
+temp_count = 0
+hum_count = 0
+
+base_amp = float(250)
+base_freq = float(1000)
+base_temp = float(60)
+base_hum = float(30)
+
+equipment_id = 1234
+
+while True:
+
+    # AMPLITUDE
+    if (amp_flag == False) and (random.random() < amp_odds):
+        amp_flag = True
+
+    if (amp_flag == True) and amp_count < 15:
+        amp_count = amp_count + 1
+        vibration_amplitude = round(base_amp * round(random.uniform(1.5, 1.8), 3), 2)
+        print ('AMPLITUDE OUTLIER')
+    else:
+        amp_count = 0
+        amp_flag = False
+        vibration_amplitude = round(base_amp * round(random.uniform(0.9, 1.1), 3), 2)
+
+    # FREQUENCY
+    if (freq_flag == False) and (random.random() < freq_odds):
+        freq_flag = True
+
+    if (freq_flag == True) and freq_count < 15:
+        freq_count = freq_count + 1
+        vibration_frequency = round(base_freq * round(random.uniform(1.5, 1.8), 3), 2)
+        print ('FREQUENCY OUTLIER')
+    else:
+        freq_count = 0
+        freq_flag = False
+        vibration_frequency = round(base_freq * round(random.uniform(0.9, 1.1), 3), 2)
+
+    # TEMPURATURE
+    if (temp_flag == False) and (random.random() < temp_odds):
+        temp_flag = True
+
+    if (temp_flag == True) and temp_count < 15:
+        temp_count = temp_count + 1
+        tempurature = round(base_temp * round(random.uniform(1.5, 1.8), 3), 2)
+        print ('TEMPERATURE OUTLIER')
+    else:
+        temp_count = 0
+        temp_flag = False
+        tempurature = round(base_temp * round(random.uniform(0.9, 1.1), 3), 2)
+
+    # HUMIDITY
+    if (hum_flag == False) and (random.random() < hum_odds):
+        hum_flag = True
+
+    if (hum_flag == True) and hum_count < 15:
+        hum_count = hum_count + 1
+        humidity = round(base_hum * round(random.uniform(1.5, 1.8), 3), 2)
+        print ('HUMIDITY OUTLIER')
+    else:
+        hum_count = 0
+        hum_flag = False
+        humidity = round(base_hum * round(random.uniform(0.9, 1.1), 3), 2)
+
+    payload = [{"equipment_id": equipment_id, "vibration_amplitude": vibration_amplitude, "vibration_frequency": vibration_frequency, "temperature": tempurature, "humidity": humidity}]
+    payload = json.dumps(payload)
+    encoded_value = b64encode(payload.encode()).decode()
+    timestamp = str(datetime.datetime.now())
+    encoded_key = b64encode(timestamp.encode()).decode()
+
+    # Initialize service client with default config file
+    streaming_client = oci.streaming.StreamClient(config, message_endpoint)
+
+    # Send the message request to streaming service
+    mesgs=[oci.streaming.models.PutMessagesDetailsEntry(value=encoded_value,key=encoded_key)]
+    put_mesgs_dets=oci.streaming.models.PutMessagesDetails(messages=mesgs)
+    put_messages_response = streaming_client.put_messages(stream_id=sid, put_messages_details=put_mesgs_dets)
+
+    # Get the data from response
+    if (put_messages_response.data.failures) == 0:
+        print ('SENT: ' + str(payload))
+    else:
+        print ('MESSAGE FAILURE: ' + str(payload))
+
+    time.sleep(1)
