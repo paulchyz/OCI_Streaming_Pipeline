858023b8d6d64bc7ba7c9ac70be49560e8702591
paulchyz <paulhchyz@gmail.com>
2023-06-02 16:27:20 -0700

    Updating SQL

--- modules/sql/STREAM_PIPELINE.sql
+++ modules/sql/STREAM_PIPELINE.sql
@@ -25,50 +25,14 @@ SELECT STREAM, KEY, PARTITION, OFFSET, BATCH_TIMESTAMP, TO_TIMESTAMP(TIMESTAMP, 
 -- STREAMDATA_LAST10_VIEW
 -- Create database view to return the most recent 10 minutes of relational data from JSON collection
 CREATE OR REPLACE VIEW STREAMDATA_LAST10_VIEW AS
-SELECT STREAM, KEY, PARTITION, OFFSET, BATCH_TIMESTAMP, TO_TIMESTAMP(TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF') TIMESTAMP, EQUIPMENT_ID, VIBRATION_AMPLITUDE, VIBRATION_FREQUENCY, TEMPERATURE, HUMIDITY from STREAMDATA,
-    JSON_TABLE (
-        STREAMDATA.JSON_DOCUMENT COLUMNS (
-            NESTED PATH '$[*]' COLUMNS (
-                STREAM VARCHAR2(40) PATH '$.stream',
-                KEY NUMBER PATH '$.key',
-                PARTITION NUMBER PATH '$.partition',
-                OFFSET NUMBER PATH '$.offset',
-                BATCH_TIMESTAMP NUMBER PATH '$.timestamp',
-                NESTED PATH '$.value[*]' COLUMNS (
-                    TIMESTAMP VARCHAR2(100) PATH '$.timestamp',
-                    EQUIPMENT_ID NUMBER PATH '$.equipment_id',
-                    VIBRATION_AMPLITUDE NUMBER PATH '$.vibration_amplitude',
-                    VIBRATION_FREQUENCY NUMBER PATH '$.vibration_frequency',
-                    TEMPERATURE NUMBER PATH '$.temperature',
-                    HUMIDITY NUMBER PATH '$.humidity'
-                )
-            )
-        )
-    ) WHERE TO_TIMESTAMP(TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF') > (SELECT MAX(TIMESTAMP) FROM STREAMDATA_VIEW) - INTERVAL '10' MINUTE ORDER BY KEY DESC;
+SELECT * FROM STREAMDATA_VIEW
+WHERE TIMESTAMP > (SELECT MAX(TIMESTAMP) FROM STREAMDATA_VIEW) - INTERVAL '10' MINUTE ORDER BY KEY DESC;
 
 -- STREAMDATA_LAST3_VIEW
 -- Create database view to return the most recent 3 minutes of relational data from JSON collection
 CREATE OR REPLACE VIEW STREAMDATA_LAST3_VIEW AS
-SELECT STREAM, KEY, PARTITION, OFFSET, BATCH_TIMESTAMP, TO_TIMESTAMP(TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF') TIMESTAMP, EQUIPMENT_ID, VIBRATION_AMPLITUDE, VIBRATION_FREQUENCY, TEMPERATURE, HUMIDITY from STREAMDATA,
-    JSON_TABLE (
-        STREAMDATA.JSON_DOCUMENT COLUMNS (
-            NESTED PATH '$[*]' COLUMNS (
-                STREAM VARCHAR2(40) PATH '$.stream',
-                KEY NUMBER PATH '$.key',
-                PARTITION NUMBER PATH '$.partition',
-                OFFSET NUMBER PATH '$.offset',
-                BATCH_TIMESTAMP NUMBER PATH '$.timestamp',
-                NESTED PATH '$.value[*]' COLUMNS (
-                    TIMESTAMP VARCHAR2(100) PATH '$.timestamp',
-                    EQUIPMENT_ID NUMBER PATH '$.equipment_id',
-                    VIBRATION_AMPLITUDE NUMBER PATH '$.vibration_amplitude',
-                    VIBRATION_FREQUENCY NUMBER PATH '$.vibration_frequency',
-                    TEMPERATURE NUMBER PATH '$.temperature',
-                    HUMIDITY NUMBER PATH '$.humidity'
-                )
-            )
-        )
-    ) WHERE TO_TIMESTAMP(TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF') > (SELECT MAX(TIMESTAMP) FROM STREAMDATA_VIEW) - INTERVAL '3' MINUTE ORDER BY KEY DESC;
+SELECT * FROM STREAMDATA_VIEW
+WHERE TIMESTAMP > (SELECT MAX(TIMESTAMP) FROM STREAMDATA_VIEW) - INTERVAL '3' MINUTE ORDER BY KEY DESC;
 
 
 -- FOR REFERENCE
